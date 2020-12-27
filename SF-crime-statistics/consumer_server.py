e4b93c2a31701ded7f0d5decf498c297f1adf4ee
Hauke Hildebrandt <hauke.hildebrandt@bluecipher.de>
2020-12-27 22:18:22 +0100

    Added consumer_server

---
+++ SF-crime-statistics/consumer_server.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from confluent_kafka import Consumer
+from confluent_kafka.admin import AdminClient
+
+TOPIC_NAME = "com.udacity.crimestatistics"
+BROKER_URL = "PLAINTEXT://localhost:9092"
+
+async def consume(topic_name):
+    c = Consumer({"bootstrap.servers": BROKER_URL, "group.id": "0"})
+    c.subscribe([topic_name])
+
+    while True:
+        messages = c.consume(5, 1.0)
+        print(f"Received {len(messages)} messages")
+        for message in messages:
+            if message is None:
+                continue
+            elif message.error() is not None:
+                continue
+            else:
+                print(f"{message.key()} => {message.value()}")
+            await asyncio.sleep(0.01)
+
+
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    client = AdminClient({"bootstrap.servers": BROKER_URL})
+
+    try:
+        asyncio.run(consume(TOPIC_NAME))
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+if __name__ == "__main__":
+    main()
